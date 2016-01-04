55d5b40d08424c86b61007c9762a21359f5d6298
zivlakmilos <zivlakmilos@gmail.com>
2016-01-04 22:14:42 +0100

    Uradjena osnovna tekstura za zid

--- functions.cpp
+++ functions.cpp
@@ -36,17 +36,9 @@ unsigned int loadModel(SDL_Surface *image, SDL_Rect imageRect)
 
 unsigned int loadModel(std::string path)
 {
-	SDL_Surface *image = IMG_Load(path);
-	SDL_DisplayFormatAlpha(tmpImage);
-    SDL_Rect tmpRect;
-    
-    SDL_Surface *tmpImage;  // Tempoary surface
-    tmpImage = SDL_CreateRGBSurface(0, tmpRect.w, tmpRect.h,
-            image->format->BitsPerPixel, image->format->Rmask,
-            image->format->Gmask, image->format->Bmask, image->format->Amask);
-    SDL_SetAlpha(image, 0, 0);
+	SDL_Surface *image = IMG_Load(path.c_str());
 
-    SDL_BlitSurface(image, &imageRect, tmpImage, &tmpRect); // copy image
+    SDL_SetAlpha(image, 0, 0);
 
     unsigned object(0);
     glGenTextures(1, &object);
@@ -57,10 +49,10 @@ unsigned int loadModel(std::string path)
     glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
     glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, tmpImage->w, tmpImage->h, 0,
-            GL_RGBA, GL_UNSIGNED_BYTE, tmpImage->pixels);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, image->w, image->h, 0,
+            GL_RGBA, GL_UNSIGNED_BYTE, image->pixels);
 
     SDL_FreeSurface(image);
-    
+
     return object;
 }
