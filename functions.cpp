6baabc9a1bd6b534e51b24c60c3d9c30378dbe85
zivlakmilos <zivlakmilos@gmail.com>
2016-01-04 19:57:56 +0100

    Ispravak skripte za git

--- functions.cpp
+++ functions.cpp
@@ -33,3 +33,34 @@ unsigned int loadModel(SDL_Surface *image, SDL_Rect imageRect)
 
     return object;
 }
+
+unsigned int loadModel(std::string path)
+{
+	SDL_Surface *image = IMG_Load(path);
+	SDL_DisplayFormatAlpha(tmpImage);
+    SDL_Rect tmpRect;
+    
+    SDL_Surface *tmpImage;  // Tempoary surface
+    tmpImage = SDL_CreateRGBSurface(0, tmpRect.w, tmpRect.h,
+            image->format->BitsPerPixel, image->format->Rmask,
+            image->format->Gmask, image->format->Bmask, image->format->Amask);
+    SDL_SetAlpha(image, 0, 0);
+
+    SDL_BlitSurface(image, &imageRect, tmpImage, &tmpRect); // copy image
+
+    unsigned object(0);
+    glGenTextures(1, &object);
+    glBindTexture(GL_TEXTURE_2D, object);
+
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, tmpImage->w, tmpImage->h, 0,
+            GL_RGBA, GL_UNSIGNED_BYTE, tmpImage->pixels);
+
+    SDL_FreeSurface(image);
+    
+    return object;
+}
