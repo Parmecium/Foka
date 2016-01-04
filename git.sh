5bd2dc5547eaa4e221bf497fe376c915d189a522
zivlakmilos <zivlakmilos@gmail.com>
2016-01-04 17:55:36 +0100

    Pocetna skripta za git

---
+++ git.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ "$1" == "init" ]
+then
+elif [ "$1" == "pull" ]
+then
+    git pull
+    git checkout aca
+    git fetch orign
+    git merge orign/master
+elif [ "$1" == "commit" ]
+    git commit -m "$2" -a
+    git push
+then
+fi
