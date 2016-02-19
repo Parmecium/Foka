de233e9a7e398e74fcf7eb4bc438442169c03cf1
AionCode <astojanovic84@gmail.com>
2016-02-19 21:24:35 +0100

    Promenio neke stvari

--- inventory.cpp
+++ inventory.cpp
@@ -23,7 +23,7 @@ class InventoryAnimator : public Ticker
 Inventory::Inventory(float width, float height, Timer *timer)
 {
     this->screen = { 0, 0, width, height };
-    this->heart = { 0, height - 100, 100, 100 };
+    this->heart = { 35, height - 100, 100, 100 };
     this->textureState = 0;
     this->textureIncrement = 1;
 
