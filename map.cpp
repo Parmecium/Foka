8e47830e29a169038303a5fbd4c66958e2e73235
AionCode <astojanovic84@gmail.com>
2016-02-21 02:48:37 +0100

    Dodao poz

--- map.cpp
+++ map.cpp
@@ -23,7 +23,7 @@ Map::Map(float width, float height, float sWidth, float sHeight)
     player2 = new Player(130, 95, timer);
     inventory = new Inventory(this->sWidth, this->sHeight, timer);
 
-    this->bgCoords = {0, 0, 1000, 1000};
+    this->bgCoords = {0, 0, 350, 350};
 
     // Test
     //this->tile.push_back(new Tile(600, 300, TILE_LEAF));
