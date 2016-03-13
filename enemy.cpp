e06848babcd830d4426edc8d099614c5cdfb45c8
Milos Zivlak <zivlakmilos@gmail.com>
2016-03-13 01:20:33 +0100

    Uradjen nov sistem tekstura za enemy i kao primer za test koriscen voodoo green

--- enemy.cpp
+++ enemy.cpp
@@ -21,13 +21,15 @@ class EnemyAnimator : public Ticker
         }
 };
 
-Enemy::Enemy(float x, float y, Timer *timer, float w, float h, int interval)
+Enemy::Enemy(float x, float y, Timer *timer, int type,
+        float w, float h, int interval)
     : Player(x, y, NULL, w, h, interval)
 {
     this->moveState.right = true;
     this->minX = x;
     this->maxX = x + 500;
     this->speed = 3.0;
+    this->type = type;
 
     this->textureIncrement = 1;
 
@@ -57,5 +59,24 @@ void Enemy::move(void)
     {
         this->moveState.right = false;
         this->moveState.left = true;
+    }
+}
+
+void Enemy::loadTexture()
+{
+    int i;
+    switch(type)
+    {
+        case ENEMY_TYPE_VOODOO:
+            this->texture.down[0] = loadModel("data/voodoo/voodoo_green1.png");
+            this->texture.down[1] = loadModel("data/voodoo/voodoo_green2.png");
+            this->texture.down[2] = loadModel("data/voodoo/voodoo_green3.png");
+            for(i = 0; i < 3; i++)
+            {
+                this->texture.up[i] = texture.down[i];
+                this->texture.left[i] = texture.down[i];
+                this->texture.right[i] = texture.down[i];
+            }
+            break;
     }
 }
--- enemy.h
+++ enemy.h
@@ -1,17 +1,21 @@
 #ifndef ENEMY_H
 #define ENEMY_H
 
+#define ENEMY_TYPE_VOODOO           0
+
 class Enemy : public Player
 {
     protected:
+        int type;
         float minX;
         float maxX;
 
     public:
-        Enemy(float x, float y, Timer *timer,
+        Enemy(float x, float y, Timer *timer, int type,
                 float w = 90.0f, float h = 107.0f, int interval = 90);
         ~Enemy(void);
         void move(void);
+        void loadTexture();
 };
 
 #endif  // ENEMY_H
