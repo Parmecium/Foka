d77c181da578d01ef7fef67582be6754333d9c86
Milos Zivlak <zivlakmilos@gmail.com>
2016-03-13 16:13:01 +0100

    Neprijatelji prate player-a, ima bug, ali radim na tome

--- enemy.h
+++ enemy.h
@@ -9,6 +9,9 @@ class Enemy : public Player
         int type;
         float minX;
         float maxX;
+        float speedX;
+        float speedY;
+        SDL_Rect seeArea;
 
     public:
         Enemy(float x, float y, Timer *timer, int type,
@@ -16,6 +19,7 @@ class Enemy : public Player
         ~Enemy(void);
         void move(void);
         void loadTexture();
+        void seeLogic(Player *player);
 };
 
 #endif  // ENEMY_H
