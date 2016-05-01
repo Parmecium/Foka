b609ddc0b1250124453cb1527ba0da9e414fb48d
Milos Zivlak <zivlakmilos@gmail.com>
2016-05-01 19:00:19 +0200

    Pojacan zvuk pozadinske muzike u mapi na MAX / 2 i smanjen menu

--- map.cpp
+++ map.cpp
@@ -107,7 +107,7 @@ void Map::loadMusic(void)
 {
     Mix_OpenAudio(25050, MIX_DEFAULT_FORMAT, 2, 2096);
     music = Mix_LoadMUS("data/muzika/menu_theme.mp3");
-    Mix_VolumeMusic(MIX_MAX_VOLUME / 4);
+    Mix_VolumeMusic(MIX_MAX_VOLUME / 2);
     Mix_PlayMusic(music, -1);
 }
 
