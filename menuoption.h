b12304885e9d5ee8a65b2b45b747deb232239a05
Milos Zivlak <zivlakmilos@gmail.com>
2016-05-02 03:50:21 +0200

    Dodati jos i manuopion.h/.cpp sto sam u proslom commit-u zaboravio :D

---
+++ menuoption.h
@@ -0,0 +1,21 @@
+#ifndef MENU_OPTION_H
+#define MENU_OPTION_H
+
+class MenuOption
+{
+    private:
+        SDL_Rect coords;
+        unsigned int texture[2];
+        bool selected;
+
+    public:
+        MenuOption(int x, int y, int w = 100, int h = 100);
+        ~MenuOption(void);
+
+        void loadTexture(std::string fileName);
+        void render(void);
+
+        void setSelected(bool value) { this->selected = value; }
+};
+
+#endif  // MENU_OPTION_H
