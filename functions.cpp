9ed6d3f0adc18cc33437deee9b0f980504b4c5b3
Milos Zivlak <zivlakmilos@gmail.com>
2016-05-14 23:00:31 +0200

    Ubacena funkcija za ucitavanje sprajta iz spritesheet-a (iz Djure), nije testirano, ne znam da li radi

--- functions.cpp
+++ functions.cpp
@@ -24,6 +24,40 @@ unsigned int loadModel(std::string path)
     return object;
 }
 
+unsigned int loadModel(std::string path, SDL_Rect imageRect)
+{
+    SDL_Surface *image = IMG_Load(path.c_str());
+    SDL_DisplayFormatAlpha(image);
+    SDL_Surface *tempImage;   // Tempoary surface
+    tempImage = SDL_CreateRGBSurface(0, imageRect.w, imageRect.h,image->format->BitsPerPixel, image->format->Rmask, image->format->Gmask, image->format->Bmask, image->format->Amask);  // Make empty surface
+    SDL_SetAlpha(image, 0, 0);
+
+    SDL_Rect tempRect;  // Tempoary rectangle for dest information
+    tempRect.x = 0;
+    tempRect.y = 0;
+    tempRect.w = imageRect.w;
+    tempRect.h = imageRect.h;
+
+    SDL_BlitSurface(image, &imageRect, tempImage, &tempRect);   // Copy image
+
+    unsigned object(0);
+    glGenTextures(1, &object);
+    glBindTexture(GL_TEXTURE_2D, object);
+
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, tempImage->w, tempImage->h, 0, GL_RGBA, GL_UNSIGNED_BYTE, tempImage->pixels);
+
+    //SDL_FreeSurface(image); // Free surface
+    SDL_FreeSurface(tempImage); // Free tempImage surface
+
+    // Return texture
+    return object;
+}
+
 int randnum(int max)
 {
     //srand(time(NULL));
--- functions.h
+++ functions.h
@@ -2,6 +2,7 @@
 #define FUNCTIONS_H
 
 unsigned int loadModel(std::string path);
+unsigned int loadModel(std::string path, SDL_Rect imageRect);
 int randnum(int max);
 
 #endif  // FUNCTIONS_H
