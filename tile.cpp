8095d29214baa41f7717460d986fae849c59bef8
Milos Zivlak <zivlakmilos@gmail.com>
2016-06-18 22:24:52 +0200

    Povecan sto jos, Acina zelja

--- tile.cpp
+++ tile.cpp
@@ -25,8 +25,8 @@ Tile::Tile(float x, float y, int type)
             this->coords.h = 150;
             break;
         case TILE_DESK:
-            this->coords.w = 100;
-            this->coords.h = 100;
+            this->coords.w = 500;
+            this->coords.h = 382;
             break;
         case TILE_TREE_FALL:
             this->coords.w = 90;
