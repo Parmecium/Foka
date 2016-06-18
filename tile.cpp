2f1f71c7da4dbd1fa9e1068b2080f94dbec9dc87
Milos Zivlak <zivlakmilos@gmail.com>
2016-06-18 22:41:29 +0200

    Podignuta glava player-a i povecan sto

--- tile.cpp
+++ tile.cpp
@@ -25,8 +25,8 @@ Tile::Tile(float x, float y, int type)
             this->coords.h = 150;
             break;
         case TILE_DESK:
-            this->coords.w = 500;
-            this->coords.h = 382;
+            this->coords.w = 150;
+            this->coords.h = 120;
             break;
         case TILE_TREE_FALL:
             this->coords.w = 90;
